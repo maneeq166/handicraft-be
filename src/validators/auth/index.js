43c92c51eea9d73b235ac927744b3f115f69faf3
faaiz251 <mohdfaaizazeem@gmail.com>
2025-11-01 15:26:15 +0530

    validating phone no with dial code

--- src/validators/auth/index.js
+++ src/validators/auth/index.js
@@ -2,40 +2,56 @@ const { body, param, query } = require("express-validator");
 
 exports.validateUserCreation = [
   body("username")
-    .notEmpty().withMessage("Username is required")
-    .isLength({ min: 3 }).withMessage("Username must be at least 3 characters"),
-  
+    .notEmpty()
+    .withMessage("Username is required")
+    .isLength({ min: 3 })
+    .withMessage("Username must be at least 3 characters"),
+
   body("email")
-    .notEmpty().withMessage("Email is required")
-    .isEmail().withMessage("Invalid email format"),
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Invalid email format"),
 
   body("password")
-    .notEmpty().withMessage("Password is required")
-    .isLength({ min: 6 }).withMessage("Password must be at least 6 characters"),
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters"),
 
   body("phoneNumber")
-    .notEmpty().withMessage("Phone number is required")
-    .isNumeric().withMessage("Phone number must contain only digits")
-    .isLength({ min: 10, max: 15 }).withMessage("Phone number must be between 10–15 digits"),
+    .notEmpty()
+    .withMessage("Phone number is required")
+    .matches(/^\+\d{10,15}$/)
+    .withMessage("Invalid phone number format"),
 
   body("location")
-    .notEmpty().withMessage("Location is required")
-    .isString().withMessage("Location must be an String"),
+    .notEmpty()
+    .withMessage("Location is required")
+    .isString()
+    .withMessage("Location must be an String"),
 
   body("role")
     .optional()
-    .isIn(["user", "admin"]).withMessage("Role must be either user or admin"),
+    .isIn(["user", "admin"])
+    .withMessage("Role must be either user or admin"),
 ];
 
 exports.validateUserLogin = [
   body("password").notEmpty().withMessage("Password is required"),
   body("email").optional().isEmail().withMessage("Invalid email format"),
-  body("phoneNumber").optional().isNumeric().withMessage("Phone number must be numeric"),
+  body("phoneNumber")
+    .optional()
+    .isNumeric()
+    .withMessage("Phone number must be numeric"),
 ];
 
 exports.validateUserRead = [
   query("email").optional().isEmail().withMessage("Invalid email format"),
-  query("phoneNumber").optional().isNumeric().withMessage("Phone number must be numeric"),
+  query("phoneNumber")
+    .optional()
+    .isNumeric()
+    .withMessage("Phone number must be numeric"),
 ];
 
 exports.validateUserUpdation = [
